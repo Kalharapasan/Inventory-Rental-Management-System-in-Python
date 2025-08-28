73a0e5f9605723bacf63f378ec6f5d2fe7c79e4e
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-08-28 15:55:44 +0530

    Update code

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ import tkinter.messagebox
 from tkinter import messagebox
 import datetime
 
+class Renttal_Inventory:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Rental Inventory Management System") 
+        self.root.geometry("1350x800+0+0")  
+        self.root.configure(background='gainsboro')
+
+        #======================================================================================Frame============================================#
+
+
+
+
+
+
+if __name__=='__main__':
+    root=Tk()
+    application =Renttal_Inventory(root)
+    root.mainloop()
+
